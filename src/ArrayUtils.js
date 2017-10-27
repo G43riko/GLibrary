66dc66f55f6a1846eacb46d8aaf8c42fe5c95c81
Gabriel Csollei <gcsollei@hotmail.com>
2017-10-27 15:51:50 +0200

    Update ArrayUtils.js

--- src/ArrayUtils.js
+++ src/ArrayUtils.js
@@ -6,7 +6,7 @@ exports.isIn = function(obj, data) {
             return true;
         }
     } else {
-        for(var i = 1 ; i < arguments.length ; i++) {
+        for (let i = 1 ; i < arguments.length ; i++) {
             if (arguments[i] === obj) {
                 return true;
             }
@@ -15,59 +15,65 @@ exports.isIn = function(obj, data) {
     return false;
 }
 
-Array.prototype.last = function() {
-    if (this.length === 0) {
-        return null;
-    }
+function generateArrayUtils(target = {}) {
+    const staticTarget = target === Array.prototype ? Array : target;
 
-    return this[this.length - 1];
-}
-
-Array.prototype.unique = function() {
-    const length = this.length;
-    const result = [];
-    const o = {};
-    for (i = 0 ; i < length ; i++) {
-        if(typeof o[this[i]] === "undefined") {
-            o[this[i]] = 1;
-            result[result.length] = this[i];
+    staticTarget.subArray = function(array, from = 0, to = array.length) {
+        const result = [];
+        const final = array.length < to ? array.length : to;
+        for (let i = from ; i < final ; i++) {
+            result[result.length] = array[i];
         }
-    }
-    return result;
-};
+        return result;
+    };
 
-Array.prototype.subArray = function(from = 0, to = this.length) {
-    const result = [];
-    const final = this.length < to ? this.length : to;
-    for (i = from ; i < final ; i++) {
-        result[result.length] = this[i];
-    }
-    return result;
-};
+    target.last = function(thisArg = this) {
+        if (thisArg.length === 0) {
+            return null;
+        }
+    
+        return thisArg[thisArg.length - 1];
+    };
+    
+    target.unique = function(thisArg = this) {
+        const length = thisArg.length;
+        const result = [];
+        const o = {};
+        for (let i = 0 ; i < length ; i++) {
+            if (typeof o[thisArg[i]] === "undefined") {
+                o[thisArg[i]] = 1;
+                result[result.length] = thisArg[i];
+            }
+        }
+        return result;
+    };
+    
+    target.swap = function(thisArg = this) {
+        const length = thisArg.length - 1;
+        const length2 = thisArg.length >> 1;
+        for (let i = 0 ; i < length2 ; i++) {
+            const tmp = thisArg[i];
+            thisArg[i] = thisArg[length - i];
+            thisArg[length - i] = tmp;
+        }
+        return thisArg;
+    };
+    
+    target.max = function(thisArg = this) {
+        return Math.max.apply(Math, thisArg);
+    };
+    
+    target.min = function(thisArg = this) {
+        return Math.min.apply(Math, thisArg);
+    };
+    
+    target.sum = function(thisArg = this) {
+        return thisArg.reduce((a, b) => a + b);
+    };
+    
+    target.avg = function(thisArg = this) {
+        return thisArg.reduce((a, b) => a + b) / thisArg.length;
+    };
 
-Array.prototype.swap = function() {
-    const length = this.length - 1;
-    const length2 = Math.floor(this.length / 2);
-    for (let i=0 ; i<length2 ; i++) {
-        const tmp = this[i];
-        this[i] = this[length - i];
-        this[length - i] = tmp;
-    }
-    return this;
+    return target;
 }
-
-Array.prototype.max = function(){
-	return Math.max.apply(Math, this);
-};
-
-Array.prototype.min = function(){
-	return Math.min.apply(Math, this);
-};
-
-Array.prototype.sum = function(){
-	return this.reduce((a, b) => a + b);
-};
-
-Array.prototype.avg = function(){
-	return this.reduce((a, b) => a + b) / this.length;
-};

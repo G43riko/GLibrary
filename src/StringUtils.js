3620755c6e2cb7cf0eb792811362054d15415682
Gabriel Csollei <gabrielcsollei@gmail.com>
2017-11-04 12:05:39 +0100

    pradané testy na StringUtils

--- src/StringUtils.js
+++ src/StringUtils.js
@@ -1,15 +1,121 @@
-const accentedCharacters  = "áäčďéíĺľňóôŕšťúýžÁÄČĎÉÍĹĽŇÓÔŔŠŤÚÝŽàèìòùáéíóúýâêîôûãñõäëïöüÿÀÈÌÒÙÁÉÍÓÚÝÂÊÎÔÛÃÑÕÄËÏÖÜŸ";
-const normalCharacters    = "aacdeillnoorstuyzAACDEILLNOORSTUYZaeiouaeiouyaeiouanoaeiouyAEIOUAEIOUYAEIOUANOAEIOUY";
 
-export class StringUtils {
-    static removeAccentedCharacters(word) {
+"use strict";
+
+const accentedLowerCharacters = "ąàáäâãåæăćčĉęèéëêĝĥìíïîĵłľńňòóöőôõðøśșşšŝťțţŭùúüűûñÿýçżźž";
+const normalLowerCharacters = "aaaaaaaaaccceeeeeghiiiijllnnoooooooossssstttuuuuuunyyczzz";
+const accentedCharacters = accentedLowerCharacters + accentedLowerCharacters.toUpperCase();
+const normalCharacters = normalLowerCharacters + normalLowerCharacters.toUpperCase();
+
+function generateStringUtils(target = {}) {
+    const isTargetPrototype = target === Array.prototype;
+    const staticTarget = isTargetPrototype ? Array : target;
+
+    staticTarget.between = function(text, key1, key2) {
+        const startPos = text.indexOf(key1);
+        const endPos = text.indexOf(key2);
+        if(startPos < 0 && endPos >= 0) {
+            return text.substring(0, endPos);    
+        } else if(endPos < 0 && startPos >= 0) {
+            return text.substring(startPos + key1.length, text.length);    
+        } else {
+            return text.substring(startPos + key1.length, endPos);
+        }
+        
+    };
+
+    staticTarget.count = function(text, key) {
+        return (text.match(new RegExp(key, "g")) || []).length;
+    };
+
+    staticTarget.repeat = function(text, count) {
+        return new Array(count + 1).join(text);
+    };
+
+    staticTarget.removeAll = function(text, words) {
+        return text.replace(new RegExp("(" + words.join("|") + ")", "g"), "");
+    };
+
+    staticTarget.template = function(text, values, start = "{{", end = "}}") {
+        start = start.replace(/[-[\]()*\s]/g, "\\$&").replace(/\$/g, '\\$');
+        end = end.replace(/[-[\]()*\s]/g, "\\$&").replace(/\$/g, '\\$');
+        const regexp = new RegExp(start + "(.+?)'" + end, "g");
+        console.log("regexp: ", regexp);
+        const matches = text.match(regexp) || [];
+
+        for(let i = 0 ; i<matches.length ; i++) {
+            const match = matches[i];
+            const key = match.substring(start.length, match.length - end.length).trim();
+            const value = values[key]
+            if (value) {
+                console.log("match: ", match);
+                console.log("key: ", key);
+                console.log("value: ", value);
+                text = text.replace(match, value);
+            }
+        }
+        return text;
+    };
+
+    target.removeAccentedCharacters = function(thisArg = this) {
         const result = [];
-        for (let i = 0 ; i < word.length ; i++) {
-            const index = accentedCharacters.indexOf(word[i]);
-            result[result.length] = index >= 0 ? normalCharacters[index] : word[i];
+        for (let i = 0 ; i < thisArg.length ; i++) {
+            const index = accentedCharacters.indexOf(thisArg[i]);
+            result[result.length] = index >= 0 ? normalCharacters[index] : thisArg[i];
         }
         return result.join("");
+    };
+
+    target.getAsciiArray = function(thisArg = this) {
+        const result = [];
+        for(let i = 0 ; i < thisArg.length ; i++) {
+            result[result.length] = thisArg[i].charCodeAt(0);
+        }
+        return result;
+    };
+
+    target.capitalize = function(thisArg = this) {
+        const result = thisArg.toLowerCase().split("");
+        // TODO first letter
+        result[0] = result[0].toUpperCase();
+        return result.join("");
+    };
+
+    target.collapseWhitespace = function(thisArg = this) {
+        return thisArg.replace(/^[\s\uFEFF\xA0]{2,}/g, '');
+    };
+
+    target.swapCase = function(thisArg = this) {
+        return thisArg.replace(/\S/g, function(char) {
+            const lowerCase = char.toLowerCase();
+            return lowerCase === char ? char.toUpperCase() : lowerCase;
+        });
+    };
+
+    target.isEmpty = function(thisArg = this) {
+        return !thisArg || /^[\s\xa0]*$/.test(thisArg);
+    };
+
+    target.lines = function(thisArg = this) {
+        return thisArg.replace(/\r\n/g, "\n").split("\n");
+    };
+    
+    return target;
+}
+
+class StringUtils {
+    static applyToPrototype() {
+        return generateStringUtils(String.prototype);
     }
+
+    static applyTo(object = {}) {
+        return generateStringUtils(object);
+    }
+    
+}
+exports.StringUtils = StringUtils;
+
+/*
+export class StringUtils {
     static getFormattedNumber(number, prefix = "+421") {
         if (number.startsWith("+")) {
             return number;
@@ -23,4 +129,50 @@ export class StringUtils {
         console.warn("Neznámy formát čísla: " + number);
         return number;
     }
+
+   
+    static count(text, key) {
+        return (text.match(new RegExp(key, "g")) || []).length;
+    }
+    static repeat(text, count) {
+        return new Array(count + 1).join(text);
+    }
+
+    static removeAll(text, words) {
+        return text.replace(new RegExp("(" + words.join("|") + ")", "g"), "");
+    }
+
+    static template (word, values, start = "{{", end = "}}") {
+        const regexp = new RegExp(start + ".*" + end, "g");
+
+        const matches = word.match(regexp) || [];
+
+        for(let i = 0 ; i<matches.length ; i++) {
+            const match = matches[i];
+            const key = match.substring(start.length, match.length - end.length).trim();
+            const value = values[key]
+            if (value) {
+                world.replace(match, value);
+            }
+        }
+    }
+
+    static underscore(word) {
+        //TODO 
+    }
+
+    static humanize(word) {
+        // TODO
+    }
+    static dasherize(word) {
+        // TODO
+    } 
+    static isUpper(word) {
+        // TODO
+    }
+
+    static isLower(word) {
+        // TODO
+    }
 }
+*/
---
+++ test/TestStringUtils.js
@@ -0,0 +1,112 @@
+var chai = require("chai");
+var expect = chai.expect;
+var should = chai.should();
+var StringUtils = require("../src/StringUtils").StringUtils;
+
+const chars = "+=§,.-?:_\"!)/()<>*\'$[]}{*&^%$#@!/\\|#&@{}^'`][~\|€¶←↓→º’‘©><§®ª`←'↓&×÷|÷×";
+const caseable = "þÞıŦŧŊEĐđNΩ";
+const numbers = "0123456789";
+const accented = "ľščťžýáíéäôňŁłŁØÆŁÐø";
+const testString = "ąàáäâãåæăćčĉęèéëêĝĥìíïîĵłľńňòóöőôõðøśșşšŝťțţŭùúüűûñÿýçżźž";
+const resultString = "aaaaaaaaaccceeeeeghiiiijllnnoooooooossssstttuuuuuunyyczzz";
+
+
+describe("string utils", function() {
+    describe("it test string functions", function(){
+        const Utils = StringUtils.applyTo();
+
+        const text = "abcdedabcdedabcdjidABdsdcdsjo";
+        it("test count", function(){
+            expect(Utils.count(text, "ab")).to.be.equal(3);
+            expect(Utils.count("gabriel", "")).to.be.equal(8);
+        });
+        it("test removeAll", function(){
+            expect(Utils.removeAll(text, ["a"])).to.be.equal("bcdedbcdedbcdjidABdsdcdsjo");
+            expect(Utils.removeAll(text, ["a", "b", "c"])).to.be.equal("deddeddjidABdsddsjo");
+        });
+
+        it("test repeat", function(){
+            expect(Utils.repeat("a", 10)).to.be.equal("aaaaaaaaaa");
+            expect(Utils.repeat("abc", 10)).to.be.equal("abcabcabcabcabcabcabcabcabcabc");
+        });
+
+        it("test between", function(){
+            expect(Utils.between(numbers, "34", "67")).to.be.equal("5");
+            expect(Utils.between(numbers, "34", "ab")).to.be.equal("56789");
+            expect(Utils.between(numbers, "ab", "67")).to.be.equal("012345");
+        });
+        it("test template", function(){
+            const params = {
+                meno: "Gabo",
+                vek: 23
+            }
+            const text = "{{meno}} je starý {{vek}} rokov";
+            expect(Utils.template(text, params)).to.be.equal("Gabo je starý 23 rokov");
+        });
+    });
+
+    describe("it appy function to String prototype", function() {
+        it("imported object contains all functions", function(){
+            should.exist(StringUtils.applyTo);
+            should.exist(StringUtils.applyToPrototype);
+        });
+
+        it("String prototype has not any utils", function(){
+            expect(String.prototype.removeAccentedCharacters).to.be.undefined;
+            expect(String.prototype.getAsciiArray).to.be.undefined;
+            expect(String.prototype.capitalize).to.be.undefined;
+            expect(String.prototype.collapseWhitespace).to.be.undefined;
+            expect(String.prototype.swapCase).to.be.undefined;
+            expect(String.prototype.isEmpty).to.be.undefined;
+            expect(String.prototype.lines).to.be.undefined;
+        });
+        
+        it("String prototype has all utils", function(){
+            StringUtils.applyToPrototype();
+            should.exist(String.prototype.removeAccentedCharacters);
+            should.exist(String.prototype.getAsciiArray);
+            should.exist(String.prototype.capitalize);
+            should.exist(String.prototype.collapseWhitespace);
+            should.exist(String.prototype.swapCase);
+            should.exist(String.prototype.isEmpty);
+            should.exist(String.prototype.lines);
+        });
+        // Strings.applyToPrototype();
+    });
+
+    describe("test new string prototype function", function() {
+        it("test removeAccentedCharacters", function(){
+            const finalTestString = testString + testString.toUpperCase();
+
+            const finalResultString = resultString + resultString.toUpperCase();
+
+            expect(finalTestString.removeAccentedCharacters()).to.be.equal(finalResultString);
+            expect(finalResultString.removeAccentedCharacters()).to.be.equal(finalResultString);
+            expect(chars.removeAccentedCharacters()).to.be.equal(chars);
+        });
+        
+        it("test swapCase", function(){
+            expect(chars.swapCase()).to.be.equal(chars);
+            expect(numbers.swapCase()).to.be.equal(numbers);
+            expect(testString.swapCase()).to.be.equal(testString.toUpperCase());
+        });
+
+        it("test isEmpty", function(){
+            expect("".isEmpty()).to.be.true;
+            expect(" ".isEmpty()).to.be.true;
+            expect("\t".isEmpty()).to.be.true;
+            expect("\xa0".isEmpty()).to.be.true;
+            expect("\t \xa0\t \xa0".isEmpty()).to.be.true;
+            expect("  ".isEmpty()).to.be.true;
+            expect("\t\t".isEmpty()).to.be.true;
+            expect("\xa0\xa0".isEmpty()).to.be.true;
+
+            expect("a".isEmpty()).to.be.false;
+            expect("0".isEmpty()).to.be.false;
+            expect("[]".isEmpty()).to.be.false;
+            expect("A".isEmpty()).to.be.false;
+            expect("{}".isEmpty()).to.be.false;
+            expect(".".isEmpty()).to.be.false;
+        });
+    });
+})

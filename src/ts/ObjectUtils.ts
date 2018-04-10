c7225a0935ac222697329698d12503400425229e
QBSW\gabriel.csollei <gabriel.csollei@qbsw.sk>
2018-04-10 12:57:12 +0200

    Pridaná funkcia ObjectUtils.byString

--- src/ts/ObjectUtils.ts
+++ src/ts/ObjectUtils.ts
@@ -30,4 +30,19 @@ export class ObjectUtils {
         }
         return true;
     };
+
+    public static byString(o: any, s: string): any {
+        s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
+        s = s.replace(/^\./, '');           // strip a leading dot
+        const a = s.split('.');
+        for (let i = 0, n = a.length; i < n; ++i) {
+            const k = a[i];
+            if (k in o) {
+                o = o[k];
+            } else {
+                return;
+            }
+        }
+        return o;
+    }
 }
